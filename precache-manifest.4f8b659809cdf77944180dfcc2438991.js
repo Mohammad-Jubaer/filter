7daba5b392282c92f4599dfa74129b7db06da894
Mohammad-Jubaer <jubaer2000@yahoo.com>
2020-09-03 17:04:29 +0600

    Updates

---
+++ precache-manifest.4f8b659809cdf77944180dfcc2438991.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8161dcd7409e8752344ba77877787b87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcf46bfabc34eda61690",
+    "url": "/static/css/main.c890a15d.chunk.css"
+  },
+  {
+    "revision": "935da952a572454280b8",
+    "url": "/static/js/2.7a62a769.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7a62a769.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcf46bfabc34eda61690",
+    "url": "/static/js/main.d6890e66.chunk.js"
+  },
+  {
+    "revision": "5333c90532c69e902e57",
+    "url": "/static/js/runtime-main.3a01e233.js"
+  }
+]);
